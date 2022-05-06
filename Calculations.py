9817e84cf88f95b5e4ae0382deb6bffbf1db1485
EC2 Default User <ec2-user@ip-10-0-26-237.ec2.internal>
2022-05-06 02:09:06 +0000

    Changes to multiple files

---
+++ Calculations.py
@@ -0,0 +1,6 @@
+fahrenheit = float(input("what temperature (in fahrenheit) would you like converted to Celsius? "))
+Celsius = (fahrenheit - 32) * 5 / 9
+
+
+#Taking the variables and putting them to work
+print(fahrenheit, "F is", Celsius, "C")
---
+++ FirstLastWelcome.py
@@ -0,0 +1,8 @@
+# Allow user to input their First Name and assign that input to a variable
+First = input("Please tell me your first name. ")
+
+# Allow user to input their Last Name and assign that input to a variable
+Last = input("Please tell me your last name. ")
+
+# Print a welcome message that includes the user's First Name and Last Name using the variables.
+print("Welcome", First, Last, "to the best cohort with the best coaches!")
